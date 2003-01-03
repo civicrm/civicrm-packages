6338e01335a890717ecb872ed3e6e87fd5731611
Martin Jansen <mj@php.net>
2003-01-03 12:04:10 +0000

    * Only "Informix" *or* "informix" are needed.
      This patch partially fixes bug #21186.
    
    
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@110139 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/ifx.php
+++ DB/ifx.php
@@ -74,7 +74,7 @@ class DB_ifx extends DB_common
      */
     function connect(&$dsninfo, $persistent = false)
     {
-        if (!DB::assertExtension('informix') || !DB::assertExtension('Informix')) {
+        if (!DB::assertExtension('informix') && !DB::assertExtension('Informix')) {
             return $this->raiseError(DB_ERROR_EXTENSION_NOT_FOUND);
         }
         $this->dsn = $dsninfo;
