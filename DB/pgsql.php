38a3119fb67e0fcedba9c4db61e17230875002d5
Rui Hirokawa <hirokawa@php.net>
2004-01-31 10:22:41 +0000

    my address changed to the official one.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@150206 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/pgsql.php
+++ DB/pgsql.php
@@ -13,7 +13,7 @@
 // | obtain it through the world-wide-web, please send a note to          |
 // | license@php.net so we can mail you a copy immediately.               |
 // +----------------------------------------------------------------------+
-// | Authors: Rui Hirokawa <rui_hirokawa@ybb.ne.jp>                       |
+// | Authors: Rui Hirokawa <hirokawa@php.net>                             |
 // |          Stig Bakken <ssb@php.net>                                   |
 // +----------------------------------------------------------------------+
 //
