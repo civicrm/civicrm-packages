ddd353fdf53e5aeea23e9918b046f427062591d4
Daniel Convissor <danielc@php.net>
2004-02-10 21:41:24 +0000

    Use upper case for RELATION_NAME in fieldflags query to make tableInfo() work.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@151024 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/ibase.php
+++ DB/ibase.php
@@ -561,7 +561,8 @@ class DB_ibase extends DB_common
                .' FROM  RDB$INDEX_SEGMENTS I'
                .' JOIN  RDB$RELATION_CONSTRAINTS R ON I.RDB$INDEX_NAME=R.RDB$INDEX_NAME'
               .' WHERE  I.RDB$FIELD_NAME=\''.$field_name.'\''
-                 .' AND R.RDB$RELATION_NAME=\''.$table_name.'\'';
+                 .' AND UPPER(R.RDB$RELATION_NAME)=\''.strtoupper($table_name).'\'';
+
         $result = ibase_query($this->connection, $sql);
         if (!$result) {
             return $this->ibaseRaiseError();
@@ -584,7 +585,7 @@ class DB_ibase extends DB_common
                      .' F.RDB$COMPUTED_SOURCE AS CSOURCE'
                .' FROM  RDB$RELATION_FIELDS R '
                .' JOIN  RDB$FIELDS F ON R.RDB$FIELD_SOURCE=F.RDB$FIELD_NAME'
-              .' WHERE  R.RDB$RELATION_NAME=\''.$table_name.'\''
+              .' WHERE  UPPER(R.RDB$RELATION_NAME)=\''.strtoupper($table_name).'\''
                 .' AND  R.RDB$FIELD_NAME=\''.$field_name.'\'';
         $result = ibase_query($this->connection, $sql);
         if (!$result) {
