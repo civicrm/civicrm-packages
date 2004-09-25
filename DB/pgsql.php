b16b8b9c5dd31d538a2fa72dd92f762cf4f634e8
Daniel Convissor <danielc@php.net>
2004-09-25 21:58:00 +0000

    * Don't anchor start of error regex.  Bug 2399.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@169228 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/pgsql.php
+++ DB/pgsql.php
@@ -243,7 +243,7 @@ class DB_pgsql extends DB_common
         static $error_regexps;
         if (!isset($error_regexps)) {
             $error_regexps = array(
-                '/^(([Rr]elation|[Ss]equence|[Tt]able)( [\"\'].*[\"\'])? does not exist|[Cc]lass ".+" not found)$/' => DB_ERROR_NOSUCHTABLE,
+                '/(([Rr]elation|[Ss]equence|[Tt]able)( [\"\'].*[\"\'])? does not exist|[Cc]lass ".+" not found)$/' => DB_ERROR_NOSUCHTABLE,
                 '/[Cc]olumn [\"\'].*[\"\'] .*does not exist/' => DB_ERROR_NOSUCHFIELD,
                 '/[Rr]elation [\"\'].*[\"\'] already exists|[Cc]annot insert a duplicate key into (a )?unique index.*/' => DB_ERROR_ALREADY_EXISTS,
                 '/(divide|division) by zero$/'          => DB_ERROR_DIVZERO,
