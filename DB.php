a0774ddb49e086e46eb0947d816eab3894a7c2d1
Daniel Convissor <danielc@php.net>
2004-12-24 05:04:10 +0000

    * Use @package_version@ in apiVersion(). * Forgot to remove old header block.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@175538 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB.php
+++ DB.php
@@ -27,32 +27,6 @@
  */
 require_once 'PEAR.php';
 
-/* vim: set expandtab tabstop=4 shiftwidth=4 foldmethod=marker: */
-// +----------------------------------------------------------------------+
-// | PHP Version 4                                                        |
-// +----------------------------------------------------------------------+
-// | Copyright (c) 1997-2004 The PHP Group                                |
-// +----------------------------------------------------------------------+
-// | This source file is subject to version 2.02 of the PHP license,      |
-// | that is bundled with this package in the file LICENSE, and is        |
-// | available at through the world-wide-web at                           |
-// | http://www.php.net/license/2_02.txt.                                 |
-// | If you did not receive a copy of the PHP license and are unable to   |
-// | obtain it through the world-wide-web, please send a note to          |
-// | license@php.net so we can mail you a copy immediately.               |
-// +----------------------------------------------------------------------+
-// | Authors: Stig Bakken <ssb@php.net>                                   |
-// |          Tomas V.V.Cox <cox@idecnet.com>                             |
-// | Maintainer: Daniel Convissor <danielc@php.net>                       |
-// +----------------------------------------------------------------------+
-//
-// $Id$
-//
-// Database independent query interface.
-
-
-require_once 'PEAR.php';
-
 // {{{ constants
 // {{{ error codes
 
@@ -443,7 +417,7 @@ class DB
      */
     function apiVersion()
     {
-        return 2;
+        return '@package_version@';
     }
 
     // }}}
