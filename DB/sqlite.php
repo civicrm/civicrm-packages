f952e1501e1f327a33a6400d929ac8395fdca2eb
Daniel Convissor <danielc@php.net>
2005-02-28 01:43:04 +0000

    * Handle more error messages:   + cannot join using column b - column not present in both tables
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@180982 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/sqlite.php
+++ DB/sqlite.php
@@ -737,6 +737,7 @@ class DB_sqlite extends DB_common
                 '/uniqueness constraint failed/' => DB_ERROR_CONSTRAINT,
                 '/may not be NULL/' => DB_ERROR_CONSTRAINT_NOT_NULL,
                 '/^no such column:/' => DB_ERROR_NOSUCHFIELD,
+                '/column not present in both tables/i' => DB_ERROR_NOSUCHFIELD,
                 '/^near ".*": syntax error$/' => DB_ERROR_SYNTAX,
                 '/[0-9]+ values for [0-9]+ columns/i' => DB_ERROR_VALUE_COUNT_ON_ROW,
             );
