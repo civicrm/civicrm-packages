33b92cdb3987b9eac231109ce8314dba8ee09f45
Daniel Convissor <danielc@php.net>
2006-04-08 12:51:00 +0000

    * In tableInfo(), fix order of params when calling mysql_query().  Bug 7343.
    git-svn-id: http://svn.php.net/repository/pear/packages/DB/trunk@210973 c90b9560-bf6c-de11-be94-00142212c4b1

--- DB/mysql.php
+++ DB/mysql.php
@@ -932,8 +932,8 @@ class DB_mysql extends DB_common
              * Probably received a table name.
              * Create a result resource identifier.
              */
-            $id = @mysql_query($this->connection,
-                                "SELECT * FROM $result LIMIT 0");
+            $id = @mysql_query("SELECT * FROM $result LIMIT 0",
+                               $this->connection);
             $got_string = true;
         } elseif (isset($result->result)) {
             /*
