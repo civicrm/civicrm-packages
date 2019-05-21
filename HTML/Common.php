4d5ba3e104ede9d45d83ff2b289032200c280325
spalmstr <spalmstr1@gmail.com>
2019-05-21 12:53:29 +0100

    Joomla_9

--- HTML/Common.php
+++ HTML/Common.php
@@ -136,15 +136,32 @@ class HTML_Common
      */
     function _getAttrString($attributes)
     {
-        $strAttr = '';
+      $strAttr = '';
 
-        if (is_array($attributes)) {
-            $charset = HTML_Common::charset();
-            foreach ($attributes as $key => $value) {
-                $strAttr .= ' ' . $key . '="' . htmlspecialchars($value, ENT_COMPAT, $charset) . '"';
+      if (is_array($attributes)) {
+          $charset = HTML_Common::charset();
+          foreach ($attributes as $key => $value) {
+            // Sometimes $value is an array.  If so, we take concatenate
+            // its elements.
+            if (is_array($value)) {
+              if (count($value) == 0) {
+                // An empty string.
+                $value1 = '';
+                $strAttr .= ' ' . $key . '="' . htmlspecialchars($value1, ENT_COMPAT, $charset) . '"';
+              }
+              else {
+                foreach ($value as $value1) {
+                  $strAttr .= ' ' . $key . '="' . htmlspecialchars($value1, ENT_COMPAT, $charset) . '"';
+                }
+              }
             }
-        }
-        return $strAttr;
+            else {
+              $strAttr .= ' ' . $key . '="' . htmlspecialchars($value, ENT_COMPAT, $charset) . '"';
+            }
+          }
+
+      }
+      return $strAttr;
     } // end func _getAttrString
 
     /**
