8effbff48e20d64f8315f8170cf3b05ab4f0118d
Tim Otten <totten@civicrm.org>
2019-08-23 14:37:11 -0700

    Remove unused SeleniumRC binaries
    Overview
    --------
    
    Remove binary copy of Selenium server, because it's not used.
    
    On a local copy of `civicrm-packages.git`, it reduces the size of the extant
    files from 33mb to 13mb.
    
    This is really just follow-up to 5.13's https://github.com/civicrm/civicrm-core/pull/13861
    
    Hopefully, some day, Selenium or a similar E2E mechanism will rise again in
    better form.  But it will probably look different
    (composer/docker/npm-ified), and there's no point in carrying the `.jar`
    file in here.

--- SeleniumRC/selenium.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-# Run selenium server with custom browser profile.
-echo; echo "Runnning selenium-server-2.53.0 "; echo;
-java -jar selenium-server/selenium-server-standalone-2.53.0.jar -firefoxProfileTemplate BrowserProfiles/firefox/
