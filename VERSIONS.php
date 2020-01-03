d085cb448d33374108e3e61fd28dc809d7212897
eileen <emcnaughton@wikimedia.org>
2020-01-03 15:15:27 +1300

    DB_DataObject - update versions file & Orig file

--- VERSIONS.php
+++ VERSIONS.php
@@ -108,7 +108,7 @@
  * Contact_Vcard_Parse           1.32.0     PHP 3.0
  * Date                          1.4.7      BSD 3-cl.
  * DB                            1.9.3      PHP 3.0
- * DB_DataObject                 1.8.12     PHP 3          local changes
+ * DB_DataObject                 1.11.3     PHP 3          local changes
  * HTML_Common                   1.2.5      PHP 3
  * HTML_QuickForm                3.2.16     PHP 3          local changes, hierselect.php from a very old version (PHP 2)
  * HTML_QuickForm_advmultiselect 1.5.1      BSD 3-cl.      local changes
