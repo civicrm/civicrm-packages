2a6eb9e0afc2dc5bf3b52cf4f89f6dcd20c21df3
Seamus Lee <seamuslee001@gmail.com>
2020-06-02 09:07:51 +1000

    Remove No longer required HTTP_Request Package

--- VERSIONS.php
+++ VERSIONS.php
@@ -114,7 +114,6 @@
  * HTML_QuickForm_advmultiselect 1.5.1      BSD 3-cl.      local changes
  * HTML_QuickForm_Controller     1.0.9      PHP 3          local changes
  * HTML_Template_IT              1.2.1      BSD 3-cl.
- * HTTP_Request                  1.4.4      BSD 3-cl.
  * Log                           1.11.5     X11
  * Mail                          1.2.0      PHP 2          local changes
  * Mail_Mime                     1.8.0      BSD 3-cl.      local changes
