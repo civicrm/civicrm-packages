71f65824e8e93ca80eec1fff051a7c9150d47e72
Seamus Lee <seamuslee001@gmail.com>
2020-06-11 08:48:47 +1000

    Remove a usage of deprecated funtion get_magic_quotes_gpc and also swich out {} for [] whne acecssing string or array offsets

--- HTML/QuickForm.php
+++ HTML/QuickForm.php
@@ -296,7 +296,7 @@ class HTML_QuickForm extends HTML_Common
         $attributes = array('action'=>$action, 'method'=>$method, 'name'=>$formName, 'id'=>$formName) + $target;
         $this->updateAttributes($attributes);
         if (!$trackSubmit || isset($_REQUEST['_qf__' . $formName])) {
-            if (1 == get_magic_quotes_gpc()) {
+            if (FALSE) {
                 $this->_submitValues = $this->_recursiveFilter('stripslashes', 'get' == $method? $_GET: $_POST);
                 foreach ($_FILES as $keyFirst => $valFirst) {
                     foreach ($valFirst as $keySecond => $valSecond) {
