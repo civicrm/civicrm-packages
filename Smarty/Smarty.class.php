1d81482abaf7d8399dcd6fcd3e923c181cfcf951
Tim Otten <totten@civicrm.org>
2024-06-19 00:43:13 -0400

    Smarty2 - Switch string evaluation to newer UserContentPolicy

--- Smarty/Smarty.class.php
+++ Smarty/Smarty.class.php
@@ -1241,9 +1241,10 @@ class Smarty
      */
     function fetch($resource_name, $cache_id = null, $compile_id = null, $display = false)
     {
-      if (preg_match('/^(\s+)?string:/', $resource_name)) {
-        $old_security = $this->security;
-        $this->security = TRUE;
+      // Do we need this forked? Seems like recipe for confusion.
+      $useSecurityPolicy = preg_match('/^(\s+)?string:/', $resource_name) && empty($smarty->security);
+      if ($useSecurityPolicy) {
+        Civi::service('civi.smarty.userContent')->enable();
       }
       try {
         static $_cache_info = [];
@@ -1357,8 +1358,8 @@ class Smarty
               error_reporting($_smarty_old_error_level);
               // restore initial cache_info
               $this->_cache_info = array_pop($_cache_info);
-              if (isset($old_security)) {
-                $this->security = $old_security;
+              if ($useSecurityPolicy) {
+                Civi::service('civi.smarty.userContent')->disable();
               }
               return TRUE;
             }
@@ -1451,24 +1452,24 @@ class Smarty
             echo smarty_core_display_debug_console($_params, $this);
           }
           error_reporting($_smarty_old_error_level);
-          if (isset($old_security)) {
-            $this->security = $old_security;
+          if ($useSecurityPolicy) {
+            Civi::service('civi.smarty.userContent')->disable();
           }
           return;
         }
         else {
           error_reporting($_smarty_old_error_level);
           if (isset($_smarty_results)) {
-            if (isset($old_security)) {
-              $this->security = $old_security;
+            if ($useSecurityPolicy) {
+              Civi::service('civi.smarty.userContent')->disable();
             }
             return $_smarty_results;
           }
         }
       }
       finally {
-        if (isset($old_security)) {
-          $this->security = $old_security;
+        if ($useSecurityPolicy) {
+          Civi::service('civi.smarty.userContent')->disable();
         }
       }
     }
